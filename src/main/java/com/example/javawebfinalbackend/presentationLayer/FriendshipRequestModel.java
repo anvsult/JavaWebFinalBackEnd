6210837c7e48c651868a50be24f7001ab931efd7
tylerfoo04 <tyfoo04@gmail.com>
2024-11-14 16:11:00 -0500

    Added response models for User, Post and Friendship

--- src/main/java/com/example/javawebfinalbackend/presentationLayer/FriendshipRequestModel.java
+++ src/main/java/com/example/javawebfinalbackend/presentationLayer/FriendshipRequestModel.java
@@ -12,20 +12,4 @@ import lombok.Setter;
 public class FriendshipRequestModel {
     private int source_id;
     private int target_id;
-
-    public int getSource_id() {
-        return source_id;
-    }
-
-    public void setSource_id(int source_id) {
-        this.source_id = source_id;
-    }
-
-    public int getTarget_id() {
-        return target_id;
-    }
-
-    public void setTarget_id(int target_id) {
-        this.target_id = target_id;
-    }
 }
--- src/main/java/com/example/javawebfinalbackend/presentationLayer/UserResponseModel.java
+++ src/main/java/com/example/javawebfinalbackend/presentationLayer/UserResponseModel.java
@@ -1,4 +1,22 @@
 package com.example.javawebfinalbackend.presentationLayer;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserResponseModel {
+    private Integer user_id;
+    private String user_name;
+    private String email;
+    private Date registered_at;
+    private Date dob;
+    private String profile_picture_url;
+    private String bio;
 }
