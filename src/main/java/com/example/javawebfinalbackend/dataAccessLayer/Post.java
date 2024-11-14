b966b526bf779339e7c9d0d26f8056abaa6d113c
REtry42 <149620675+REtry42@users.noreply.github.com>
2024-11-14 14:02:18 -0500

    Added this to the data layer

--- src/main/java/com/example/javawebfinalbackend/dataAccessLayer/Post.java
+++ src/main/java/com/example/javawebfinalbackend/dataAccessLayer/Post.java
@@ -1,4 +1,29 @@
 package com.example.javawebfinalbackend.dataAccessLayer;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id;
+
+    private String post_image_url;
+    private String post_description;
+    private LocalDateTime create_at;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
 }
