71d5a3659e8fe827e27ccbdbc8648b5cdede5ca2
tylerfoo04 <tyfoo04@gmail.com>
2024-11-14 16:04:09 -0500

    Added request models for User, Post and Friendship

--- src/main/java/com/example/javawebfinalbackend/presentationLayer/FriendshipRequestModel.java
+++ src/main/java/com/example/javawebfinalbackend/presentationLayer/FriendshipRequestModel.java
@@ -1,10 +1,14 @@
 package com.example.javawebfinalbackend.presentationLayer;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class FriendshipRequestModel {
     private int source_id;
     private int target_id;
--- src/main/java/com/example/javawebfinalbackend/presentationLayer/UserRequestModel.java
+++ src/main/java/com/example/javawebfinalbackend/presentationLayer/UserRequestModel.java
@@ -1,12 +1,16 @@
 package com.example.javawebfinalbackend.presentationLayer;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 public class UserRequestModel {
     private String user_name;
     private String email;
@@ -15,51 +19,4 @@ public class UserRequestModel {
     private String profile_picture_url;
     private String bio;
 
-    public String getUser_name() {
-        return user_name;
-    }
-
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword_hash() {
-        return password_hash;
-    }
-
-    public void setPassword_hash(String password_hash) {
-        this.password_hash = password_hash;
-    }
-
-    public Date getDob() {
-        return dob;
-    }
-
-    public void setDob(Date dob) {
-        this.dob = dob;
-    }
-
-    public String getProfile_picture_url() {
-        return profile_picture_url;
-    }
-
-    public void setProfile_picture_url(String profile_picture_url) {
-        this.profile_picture_url = profile_picture_url;
-    }
-
-    public String getBio() {
-        return bio;
-    }
-
-    public void setBio(String bio) {
-        this.bio = bio;
-    }
 }
